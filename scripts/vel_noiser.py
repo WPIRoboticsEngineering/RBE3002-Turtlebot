1e48deb4aa0d21fc43521ba18ce5b2a0b9eb0827
Alex Tacescu <alextac98@gmail.com>
2020-10-26 18:06:21 -0700

    Modify launch file to use remapping topics and modify offset formula for the velocity noiser

--- scripts/vel_noiser.py
+++ scripts/vel_noiser.py
@@ -4,19 +4,21 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
 
-noise_multiplier = 1.01
+# Offset is measured by how many meters the robot will drift 
+# per meter of distance traveled
+offset = 0.1 # meters per meter
 
 def callback(data):
     
     new_vel = data
-    new_vel.angular.z = new_vel.angular.z * noise_multiplier
+    new_vel.angular.z = new_vel.angular.z + new_vel.linear.x * 2 * offset
     pub.publish(new_vel)
 
 if __name__ == '__main__':
     rospy.init_node('vel_noise', anonymous=True)
 
-    rospy.Subscriber("/cmd_vel", Twist, callback)
-    pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+    rospy.Subscriber("cmd_vel", Twist, callback)
+    pub = rospy.Publisher("cmd_vel_robot", Twist, queue_size=1)
 
     rospy.spin()
 
