ec5477e8adca7b30c36853438b50b5420db069cc
Alex Tacescu <alextac98@gmail.com>
2020-10-26 21:22:12 -0700

    Add comments and documentation for version 1

--- scripts/vel_noiser.py
+++ scripts/vel_noiser.py
@@ -5,12 +5,14 @@ import numpy as np
 from geometry_msgs.msg import Twist
 
 # Offset is measured by how many meters the robot will drift 
-# per meter of distance traveled
+# per meter of distance traveled (estimate)
 offset = 0.1 # meters per meter
 
 def callback(data):
     
     new_vel = data
+    # Formula below estimates the curve, uses chord length instead of arc length to simplify equation
+    # This should be ok, as it is only an estimate
     new_vel.angular.z = new_vel.angular.z + new_vel.linear.x * 2 * offset
     pub.publish(new_vel)
 
